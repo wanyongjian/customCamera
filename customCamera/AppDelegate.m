03f6a3bbc0f07ae100e2295c01dd8072247fdf78
yongjian.wan <yongjian.wan@ecovacs.com>
2017-11-17 10:56:14 +0800

    filter

--- customCamera/AppDelegate.m
+++ customCamera/AppDelegate.m
@@ -7,6 +7,8 @@
 //
 
 #import "AppDelegate.h"
+#import "FilterController.h"
+#import "ViewController.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +19,10 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    self.window = [[UIWindow alloc]initWithFrame:[UIScreen mainScreen].bounds];
+    self.window.backgroundColor = [UIColor whiteColor];
+    self.window.rootViewController = [[FilterController alloc]init];
+    [self.window makeKeyAndVisible];
     return YES;
 }
 
---
+++ customCamera/FilterController.m
@@ -0,0 +1,38 @@
+//
+//  FilterController.m
+//  customCamera
+//
+//  Created by wanyongjian on 2017/11/17.
+//  Copyright © 2017年 eco. All rights reserved.
+//
+
+#import "FilterController.h"
+
+@interface FilterController ()
+
+@end
+
+@implementation FilterController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.view.backgroundColor = [UIColor grayColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
